97d06cbc61cf02f048f62a9bd5f0a9ff67f8d26e
yasaisen <yasaisenkiri@gmail.com>
2025-04-09 17:00:56 +0800

    fix log probability

--- trainer/singleStepPPOTrainer/building_singleStepPPOTrainer.py
+++ trainer/singleStepPPOTrainer/building_singleStepPPOTrainer.py
@@ -212,6 +212,8 @@ class SingleStepPPOTrainer:
             
         log_print(self.state_name, f"[{highlight()}]" + '='*43)
         log_print(self.state_name, f"[{highlight()}] new:{len(policy_new_log_probs.tolist()[0])} / old:{len(policy_old_log_probs)}")
+        print(policy_new_log_probs.tolist()[0])
+        print(policy_old_log_probs)
         for new, old in zip(policy_new_log_probs.tolist()[0], policy_old_log_probs):
             log_print(self.state_name, f"[{highlight()}] old: {old} / new: {new} / d: {new / old} / diff: {new - old}")
         log_print(self.state_name, f"[{highlight()}]" + '='*43)
